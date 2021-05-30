853185e9df4750a3b491f148f5105fa713de88e8
ahmadok <ahmad.okosh@mail.huji.ac.il>
2021-05-30 16:31:22 +0300

    Designed look and semi-functionality of ordering sandwich

--- app/src/main/java/com/ahmadok/sandwichstand/MainActivity.kt
+++ app/src/main/java/com/ahmadok/sandwichstand/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.ahmadok.sandwichstand
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -43,18 +47,35 @@ class MainActivity : AppCompatActivity() {
 //    companion object {
 //        private const val TAG = "MainActivity"
 //    }
+    var displayMessageTextView : TextView? = null
+    var placeOrderButton : Button? = null
+    var editOrderButton : Button? = null
+
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data: Intent ? = result.data
+            val userName : String? = data?.getStringExtra("username")
+            displayMessageTextView?.text = getString(R.string.display_message, userName)
+            placeOrderButton?.isEnabled = false
+            editOrderButton?.isEnabled = true
+        }
+        else{
+            displayMessageTextView?.text = getString(R.string.display_message_default)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val editOrderButton : Button = findViewById(R.id.editOrderButton)
-        val placeOrderButton : Button = findViewById(R.id.placeOrderButton)
+        val editOrderButton: Button = findViewById(R.id.editOrderButton)
+        val placeOrderButton: Button = findViewById(R.id.placeOrderButton)
+        displayMessageTextView = findViewById(R.id.displayMessageTextView)
         editOrderButton.isEnabled = false
 
         placeOrderButton.setOnClickListener {
-
+            val intent = Intent(this, PlaceOrderActivity::class.java)
+            resultLauncher.launch(intent)
         }
-
-
     }
+
 }
